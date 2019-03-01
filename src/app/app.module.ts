ac7bc24f59a468e6337fba88fe762c8f17b29e7c
subhomoyprojects@gmail.com <subhomoyprojects@gmail.com>
2019-03-01 19:42:02 +0530

    add matchip and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MetarialModule // Metarial Module Add
   ],
   providers: [],
--- src/app/metarial/metarial.module.ts
+++ src/app/metarial/metarial.module.ts
@@ -2,12 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatChipsModule} from '@angular/material/chips'; 
+import {MatAutocompleteModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MetarialModule { }

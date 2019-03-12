f6e4c7f74e8714431eabdb3ae7624571f4634b8f
subhomoyprojects@gmail.com <subhomoyprojects@gmail.com>
2019-03-12 11:55:32 +0530

    add component routing

--- src/app/component/component-routing.module.ts
+++ src/app/component/component-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { LocationComponent } from './location/location.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'location', component: LocationComponent }
 ];
 
 @NgModule({
--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { LocationComponent } from './location/location.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     LoginComponent,
     SignupComponent,
-    LocationComponent
+    LocationComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,

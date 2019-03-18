4d567980d64120aead93c92976b1dd4ac0edfd55
subhomoyprojects@gmail.com <subhomoyprojects@gmail.com>
2019-03-18 12:48:46 +0530

    add routing of other-services component

--- src/app/component/component-routing.module.ts
+++ src/app/component/component-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { FlooringComponent } from './flooring/flooring.component';
+import { OtherServicesComponent } from './other-services/other-services.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'flooring', component: FlooringComponent }
+  { path: 'flooring', component: FlooringComponent },
+  {path: 'other-services', component: OtherServicesComponent}
 ];
 
 @NgModule({
